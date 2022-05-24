e265d250f8a52a34787aeaafb94c8c3348b17b06
Pratik Shah <pratishah@deloitte.com>
2022-05-24 20:58:49 +0530

    added my firebase authsetup

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,12 +7,12 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyBXZDWGPQC6IzxakKPbrAGSX142q04YUNM",
+  authDomain: "mart-e4650.firebaseapp.com",
+  projectId: "mart-e4650",
+  storageBucket: "mart-e4650.appspot.com",
+  messagingSenderId: "922406893967",
+  appId: "1:922406893967:web:47f8139498460520edb3fa"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
